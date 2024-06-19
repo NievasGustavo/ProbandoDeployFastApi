957d68286ab6f77db6c9dfe20941d32c33ad107a
GustavoNievas <108030873+NievasGustavo@users.noreply.github.com>
2024-06-19 18:08:22 -0300

    Again uri fix

--- db/connect.py
+++ db/connect.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 # db_client = MongoClient().local | localhost
 
-db_client = MongoClient("mongodb+srv://gustavonievas1:gus123@cluster0.mfxwxdn.mongodb.net/Cluster0?retryWrites=true&w=majority").test
+db_client = MongoClient("mongodb+srv://gustavonievas1:gus123@cluster0.mfxwxdn.mongodb.net/Cluster0?retryWrites=true&w=majority", serverSelectionTimeoutMS=60000, connectTimeoutMS=60000).test
